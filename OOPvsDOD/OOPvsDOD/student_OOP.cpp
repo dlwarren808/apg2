fdc82f8c503369385bc22e0f6319a5dfcb55e2a9
dlwarren808 <46685424+dlwarren808@users.noreply.github.com>
2019-04-29 12:54:16 -0500

    a

---
+++ OOPvsDOD/OOPvsDOD/student_OOP.cpp
@@ -0,0 +1,96 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+
+class Student
+{
+	int id;
+	char name, major, email;
+
+public:
+
+	void create(Student *students, int array_size)
+	{
+		for (int i = 0; i < array_size; i++)
+		{
+			students[i].id = i;
+
+			printf("enter name:");
+			scanf("%c", &students[i].email);
+
+			printf("enter major:\n");
+			scanf("%c", &students[i].major);
+
+			printf("enter email:\n");
+			scanf("%c", &students[i].name);
+
+			printf("student enrolled:\n\n%c\n%d\n%c\n%c\n", students[i].name, students[i].id, students[i].major, students[i].email);
+		}
+	}
+
+	void destroy(Student *students)
+	{
+		int i_d = 0;
+		printf("enter student id:\n");
+		scanf("%d", &i_d);
+		students[i_d].name = NULL;
+		students[i_d].id = 0;
+		students[i_d].major = NULL;
+		students[i_d].email = NULL;
+	}
+
+	void print(Student *students)
+	{
+		int i_d = 0;
+		printf("enter student id:\n");
+		scanf("%d", &i_d);
+		printf("%c\n%d\n%c\n%c\n", students[i_d].name, students[i_d].id, students[i_d].major, students[i_d].email);
+	}
+};
+
+void make_obj(Student *students, int array_size)
+{
+	Student obj;
+	obj.create(students, array_size);
+	for(int i = 0; i < array_size; i++)
+	students[i] = obj;
+}
+
+void destroy(Student *students)
+{
+	int i_d = 0;
+	printf("enter student id:\n");
+	scanf("%d", &i_d);
+	students[i_d].destroy(students);
+}
+
+void print(Student *students)
+{
+	int i_d = 0;
+	printf("enter student id:\n");
+	scanf("%d", &i_d);
+	printf("%c\n%d\n%c\n%c\n", students[i_d].name, students[i_d].id, students[i_d].major, students[i_d].email);
+}
+
+int main()
+{
+	int add_del = 0;
+	int array_size = 0;
+	Student *students = (Student*)malloc(sizeof(Student)*array_size);
+
+	printf("enter number of students to enroll: ");
+	scanf("%d", &array_size);
+
+	while (add_del != 4)
+	{
+		printf("enter 1 to add student, 2 to delete student, 3 to print student, or 4 to exit:\n");
+		scanf("%d", &add_del);
+
+		if (add_del == 1)make_obj(students, array_size);
+		else if (add_del == 2) destroy(students);
+		else if (add_del == 3) print(students);
+	}
+
+	getchar();
+	return 0;
+}
